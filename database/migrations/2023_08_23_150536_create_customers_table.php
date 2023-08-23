57146bfa88b1ed329a46329e45c5dae67e5b311b
Vahid Reyazat <vreyazat@gmail.com>
2023-08-23 19:10:38 +0330

    setup validation composer

---
+++ database/migrations/2023_08_23_150536_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname')->unique();
+            $table->string('lastname')->unique();
+            $table->timestamp('date_of_birth')->unique();
+            $table->string('email')->unique();
+            $table->string('phonenumber')->nullable();
+            $table->string('bank_account')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
